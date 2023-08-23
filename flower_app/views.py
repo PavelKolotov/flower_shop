3489fb935e15bd92013075046127dfcedf6aff15
Pavel Kolotov <61968036+PavelKolotov@users.noreply.github.com>
2023-08-23 19:44:21 +0300

    add paginator in catalog

--- flower_app/views.py
+++ flower_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Bouquet
 
 def index(request):
@@ -8,11 +9,22 @@ def index(request):
     return render(request, 'index.html', context)
 
 def catalog_api(request):
+    flower_list = Bouquet.objects.all()
+    paginator = Paginator(flower_list, 6)
+
+    page = request.GET.get('page')
+    try:
+        flowers = paginator.page(page)
+    except PageNotAnInteger:
+        flowers = paginator.page(1)
+    except EmptyPage:
+        flowers = paginator.page(paginator.num_pages)
+
     return render(
         request,
         template_name='catalog.html',
         context={
-            'bouquets': Bouquet.objects.all()
+            'flowers': flowers
         }
     )
 
